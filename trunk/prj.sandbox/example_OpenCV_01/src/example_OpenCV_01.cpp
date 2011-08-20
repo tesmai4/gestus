1733b75af2d83774f075cd09df96a9242eb31324
Vassili.Postnikov@gmail.com <Vassili.Postnikov@gmail.com@4e61fc8f-d329-d5b6-c163-6db41fdbb475>
2011-08-20 21:36:35 +0000

    just comments

--- trunk/prj.sandbox/example_OpenCV_01/src/example_OpenCV_01.cpp
+++ trunk/prj.sandbox/example_OpenCV_01/src/example_OpenCV_01.cpp
@@ -13,11 +13,11 @@ string testdata( const char* relative_file_name )
 
 int main(int argc, char **argv) 
 {
-	main1(argc,argv);
-	main2(argc,argv);
-	main3(argc,argv);
-	main4(argc,argv);
-	main5(argc,argv);
+	main1(argc,argv); // starter, load+blur+show
+	main2(argc,argv); // detect face from still foto
+	main3(argc,argv); // detect face from web-cam
+	main4(argc,argv); // cvTreshold(), cvFindContours()
+	main5(argc,argv); // template selection & tracking, mouse response
 
 	return 0;
 }
