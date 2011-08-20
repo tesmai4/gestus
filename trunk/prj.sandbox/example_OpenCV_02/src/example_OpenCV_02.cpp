e74b00db89bfdaf794db06f2bbd6652727e3829c
Vassili.Postnikov@gmail.com <Vassili.Postnikov@gmail.com@4e61fc8f-d329-d5b6-c163-6db41fdbb475>
2011-08-20 12:18:13 +0000

    new: example_OpenCV_02; 02_01 - playing with canny and hough

---
+++ trunk/prj.sandbox/example_OpenCV_02/src/example_OpenCV_02.cpp
@@ -0,0 +1,23 @@
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include "example.h"
+using namespace std;
+
+string testdata( const char* relative_file_name )
+{
+	char buf[1024]={0};
+	GetModuleFileName(NULL, buf, sizeof(buf)-1);// win32 - ok...
+	return string(buf) + "\\..\\..\\..\\testdata\\" + string(relative_file_name);
+}
+
+int main(int argc, char **argv) 
+{
+	main1(argc,argv);
+	//main2(argc,argv);
+	//main3(argc,argv);
+	//main4(argc,argv);
+	//main5(argc,argv);
+
+	return 0;
+}
